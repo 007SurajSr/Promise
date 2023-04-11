991eede5988ffdcbf49e9c50b1f4e2d380f893ca
Suraj076 <8700suraj@gmail.com>
2023-04-11 20:28:13 +0530

    a1

---
+++ Bom1.js
@@ -0,0 +1,9 @@
+// console.log("Printing before alert....");     // Alert
+// const result = alert("Kindly be sincere and complete your assignments.");
+// console.log(result);
+// console.log("Printing after alert......");
+
+console.log("Printing before Alert .....");
+const result = prompt("Are you happy with your efforts?");
+console.log(typeof result);
+console.log("Printing after alert.....");
---
+++ sumself.js
@@ -0,0 +1,5 @@
+async function sum(a,b) {
+    return a+b;
+ }
+ const  result = sum(35,40);
+ console.log(result);
