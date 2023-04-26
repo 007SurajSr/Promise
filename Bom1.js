1ee2d06a8669255ba586113f7cbcab19704fe139
Suraj076 <8700suraj@gmail.com>
2023-04-26 19:59:43 +0530

    1.1

--- Bom1.js
+++ Bom1.js
@@ -3,7 +3,23 @@
 // console.log(result);
 // console.log("Printing after alert......");
 
-console.log("Printing before Alert .....");
-const result = prompt("Are you happy with your efforts?");
-console.log(typeof result);
-console.log("Printing after alert.....");
+// console.log("Printing before Alert .....");       //Prompt
+// const result = prompt("Are you happy with your efforts?");
+// console.log(result);
+// console.log(typeof result);
+// console.log("Printing after alert.....");
+
+http://localhost:8080/dummy.html?id=12&name=suraj#demo
+
+console.log(location.href);
+console.log(location.protocol);
+console.log(location.port);
+console.log(location.origin);
+console.log(location.pathname);
+console.log(location.search);
+console.log(location.hash);
+
+const result = confirm("Do you want to navigate to dummy html page?");
+if(result) {
+    location.href = "http://localhost:8080/dummy.html";
+} 
